fc45b2ca461910838cd876a777aa4ef47e0a0024
Mahsa <aghaie.mahsa@gmail.com>
2022-05-18 16:25:13 -0600

    feat: add docstring for devide function

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ def add(num1,num2):
     """Adds two numbers provided as arguments
     """
     return num1 + num2
-    
+
 def divide(num1,num2):
+    """divide two numbers provided as arguments
+
+    Args:
+        num1 (_type_): _description_
+        num2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
     return num1/num2
